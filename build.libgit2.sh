e06477508dd5839c7724b3cc8a7b1186b971446e
Eddy Moulton <eddy.moulton@octopus.com>
2026-04-13 16:24:08 +1000

    Add libssh2

--- build.libgit2.sh
+++ build.libgit2.sh
@@ -9,6 +9,8 @@ ARCH=`uname -m`
 PACKAGEPATH="nuget.package/runtimes"
 OSXARCHITECTURE=$ARCH
 
+EXTRA_CMAKE_FLAGS=""
+
 if [[ $OS == "Darwin" ]]; then
     USEHTTPS="ON"
     if [[ $RID == "osx-arm64" ]]; then
@@ -28,11 +30,12 @@ export _BINPATH=`pwd`
 
 cmake -DCMAKE_BUILD_TYPE:STRING=Release \
       -DBUILD_TESTS:BOOL=OFF \
-      -DUSE_SSH=exec \
+      -DUSE_SSH=ON \
       -DLIBGIT2_FILENAME=git2-$SHORTSHA \
       -DCMAKE_OSX_ARCHITECTURES=$OSXARCHITECTURE \
       -DUSE_HTTPS=$USEHTTPS \
       -DUSE_BUNDLED_ZLIB=ON \
+      $EXTRA_CMAKE_FLAGS \
       ..
 cmake --build .
 
@@ -53,3 +56,28 @@ rm -rf $PACKAGEPATH/$RID
 mkdir -p $PACKAGEPATH/$RID/native
 
 cp libgit2/build/libgit2-$SHORTSHA.$LIBEXT $PACKAGEPATH/$RID/native
+
+# Bundle libssh2 shared library alongside libgit2
+LIBGIT2_PATH="$PACKAGEPATH/$RID/native/libgit2-$SHORTSHA.$LIBEXT"
+
+if [[ $OS == "Darwin" ]]; then
+    echo "macOS: libssh2 sourced from global installation"
+else
+    # Linux: find libssh2 via ldd
+    LIBSSH2_PATH=$(ldd "$LIBGIT2_PATH" | grep libssh2 | awk '{print $3}')
+    if [[ -z "$LIBSSH2_PATH" ]]; then
+        echo "ERROR: libgit2 does not appear to link against libssh2"
+        exit 1
+    fi
+
+    LIBSSH2_BASENAME=$(basename "$LIBSSH2_PATH")
+
+    echo "Bundling $LIBSSH2_BASENAME from $LIBSSH2_PATH"
+    cp "$LIBSSH2_PATH" "$PACKAGEPATH/$RID/native/$LIBSSH2_BASENAME"
+
+    # Set RPATH so libgit2 finds libssh2 in the same directory at runtime
+    patchelf --set-rpath '$ORIGIN' "$LIBGIT2_PATH"
+fi
+
+echo "Contents of $PACKAGEPATH/$RID/native/:"
+ls -la "$PACKAGEPATH/$RID/native/"
---
+++ libssh2-wincng-triplets/arm64-windows.cmake
@@ -0,0 +1,2 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+include("${CMAKE_CURRENT_LIST_DIR}/wincng-options.cmake")

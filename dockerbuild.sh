57e06ec8488a7bf9ad28034b337932bac275e400
Eddy Moulton <eddy.moulton@octopus.com>
2026-05-13 10:22:19 +1000

    PR comments

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -14,20 +14,18 @@ else
     platform="linux/amd64"
 fi
 
-# Choose the Dockerfile (and OpenSSL 1.1 base image) based on RID and variant.
-build_args=()
+# Choose the Dockerfile based on RID and variant.
 if [[ $RID == linux-musl* ]]; then
     dockerfile="Dockerfile.linux-musl"
 elif [[ "$VARIANT" == "openssl1.1" ]]; then
     # Built on bullseye against OpenSSL 1.1, libssh2 statically linked.
-    dockerfile="Dockerfile.linux-static-libssh2"
-    build_args+=(--build-arg "BASE_IMAGE=debian:bullseye-slim")
+    dockerfile="Dockerfile.linux-openssl1.1"
 else
     # Default: built on bookworm against OpenSSL 3, libssh2 bundled as a separate .so.
     dockerfile="Dockerfile.linux"
 fi
 
-docker buildx build --platform "$platform" --load -t "$RID" -f "$dockerfile" "${build_args[@]}" .
+docker buildx build --platform "$platform" --load -t "$RID" -f "$dockerfile" .
 docker run --platform "$platform" -t -e RID=$RID -e OPENSSL_VARIANT="$VARIANT" --name="$RID" "$RID"
 docker cp "$RID":/nativebinaries/nuget.package/runtimes nuget.package
 docker rm "$RID"
